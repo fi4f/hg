623de994f39b6362698ade7990135989505a0b90
shawn-mcgee <s.mcgeek07@gmail.com>
2026-01-09 22:07:14 -0700

    Fixed an issue with default canvas

--- src/canvas.ts
+++ src/canvas.ts
@@ -4,8 +4,8 @@ export const Canvas = {
       canvas.style.position = "absolute";
       canvas.style.top      = "0";
       canvas.style.left     = "0";
-      canvas.style.width    = "dvw";
-      canvas.style.height   = "dvh";
+      canvas.style.width    = "100dvw";
+      canvas.style.height   = "100dvh";
     return document.body.appendChild(canvas);
   }
 }
